a3a537ed1800166e12336d91ae61cb088f7263a5
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2021-03-30 22:55:31 +0300

    Create homework-2.py

---
+++ Python-is-easy/homework-2.py
@@ -0,0 +1,22 @@
+# Info about my favourite song
+
+Song = 'Pey sarce'
+Year = 1989
+HasVideo = True
+
+
+def song(song):
+    return song
+
+
+def year(year):
+    return year
+
+
+def hasVideo(boolean):
+    return boolean
+
+
+print(song(Song))
+print(year(Year))
+print(hasVideo(HasVideo))
