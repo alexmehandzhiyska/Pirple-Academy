c0ccadfe04c316bcfee903d8306d531a264baeab
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2021-03-30 22:54:56 +0300

    Create homework-01.py

---
+++ Python-is-easy/homework-01.py
@@ -0,0 +1,30 @@
+#Info about my favourite song
+
+song_name = 'Pey sarce'
+
+"""Info about
+    the artist"""
+artist = 'Georgi Hristov'
+nationality = 'Bulgarian'
+year_born = 1964
+record_label = 'Silvernoise-Balkanton'
+
+"""Info about
+    the song"""
+
+genre = 'pop'
+duration_in_seconds = 190
+duration_in_minutes = 3.10
+release_year = 1989
+album = 'Golden songs'
+has_video = True
+video_release_year = 1990
+youtube_views = 400520
+youtube_likes = 3450 
+facebook_views = 900907
+facebook_likes = 2345 
+goodwall_views = 500000
+goodwall_likes = 2090
+linkedin_views = 2000
+linkedin_views = 980 
+
