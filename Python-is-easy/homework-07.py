9e3f89fd7078b75068b05132ea7c1224956a350c
Alexandrina Mehandzhiyska <73063309+alexmehandzhiyska@users.noreply.github.com>
2021-03-30 22:59:53 +0300

    Create homework-07.py

---
+++ Python-is-easy/homework-07.py
@@ -0,0 +1,5 @@
+#Info about my favourite song
+
+song_info = {'song_name': 'Pey sarce', 'artist': 'Georgi Hristov', 'genre': 'pop', 'duration_in_seconds': 190, 'duration_in_minutes': 3.10, 'release_year': 1989, 'album': 'Golden songs', 'has_video': True}
+
+print(song_info)
